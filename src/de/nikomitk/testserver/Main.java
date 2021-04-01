c8777b04cce91d0d7a6f2228feb5ab7ef35d11e6
niko <nikobo2003@gmail.com>
2021-04-01 16:46:16 +0200

    added port argument, but not tested

--- src/de/nikomitk/testserver/Main.java
+++ src/de/nikomitk/testserver/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package de.nikomitk.testserver;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -10,7 +10,12 @@ import java.net.Socket;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    ServerSocket serverSocket = new ServerSocket(5000);
+        int port = 5000;
+        if(args.length >0 && args[0].length()<10){
+            port = Integer.parseInt(args[0]);
+        }
+	    ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server running on port: " + port);
 	    while(true){
 	        Socket s = serverSocket.accept();
             BufferedReader fromClient = new BufferedReader(new InputStreamReader(s.getInputStream()));
